badae4ea2a5eca6b2dd3d491c8624c79efd30085
Sakshi Dwivedi <sakshidwivedi406@gmail.com>
2026-09-19 11:10:23 +0530

    product add issue fixed

--- backend/src/modules/admin/validators/catalog.validator.js
+++ backend/src/modules/admin/validators/catalog.validator.js
@@ -75,7 +75,7 @@ export const createProductSchema = Joi.object({
     name: productBaseSchema.name.required(),
     price: productBaseSchema.price.required(),
     categoryId: productBaseSchema.categoryId.required(),
-    vendorId: objectId.optional(),
+    vendorId: objectId.allow(null, '').optional(),
 });
 
 export const updateProductSchema = Joi.object(productBaseSchema).min(1);
